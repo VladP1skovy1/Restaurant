41f5f11683dcc9e66cb7a354c71824b5cecb1fbd
Vladyslav <103837346+VladP1skovy1@users.noreply.github.com>
2022-04-23 15:25:53 +0300

    Finish
    fixing some bags

--- js/main.js
+++ js/main.js
@@ -19,26 +19,12 @@ function OpenSearchInput(){
 
 function OpenPhoneMenu(){
     BurgerButton.style.display = "none";
-    // PhoneMenu.style.display = "block";
-    // PhoneMenu.classList.toggle("_active");
-    // document.body.classList.toggle("_lock");
     PhoneMenu.style.left = "0%";
     document.body.style.overflow = "hidden"
 
 }
 function ClosePhoneMenu(){
-    // if(window.innerWidth <= 720){
-    //     BurgerButton.style.display = "flex";
-    // }
     document.body.style.overflow = "auto"
     BurgerButton.style.display = "flex";
-    // PhoneMenu.style.display = "none";
     PhoneMenu.style.left = "-100%";
 }
-// window.addEventListener("orientationchange", function() {
-//      if(window.innerWidth <= 770){
-//          BurgerButton.style.display = "none";
-//      }else if(window.innerWidth >770){
-//         BurgerButton.style.display = "flex";
-//      }
-//     }, false);
